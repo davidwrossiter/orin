8e7fe49ebeb7bb611485616000cbffb239849668
davidwrossiter <davewrossiter@gmail.com>
2024-05-29 16:44:32 +0100

    add tailwind config and link to Warren's LinkedIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center items-center w-full h-screen">
+        <h1 className="font-redditMono px-12">
+          Orin Design is currently undergoing maintenance. If you have any
+          questions you can contact Warren{" "}
+          <span>
+            <a
+              href="https://www.linkedin.com/in/warren-g-rossiter/"
+              target="_blank"
+              className="underline"
+            >
+              here
+            </a>
+          </span>
+          .
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        redditMono: ["Reddit Mono", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
