3351c748520c3337096a7e9b93f188d068d7075c
davidwrossiter <davewrossiter@gmail.com>
2024-06-17 09:24:12 +0100

    add anchor to text in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ function Header() {
   return (
     <header className=" bg-orin-off-white w-full flex justify-center">
       <div className="max-w-[1156px] flex flex-row w-full justify-between mx-6 py-4 lg:mx-[142px]">
-        <p className="font-inter text-xl font-semibold">Orin Design</p>
+        <a href="/" className="font-inter text-xl font-semibold">
+          Orin Design
+        </a>
       </div>
     </header>
   );
